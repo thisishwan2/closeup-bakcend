0c3f9983d6a1387ab2be4f1e86e4687c31553de4
thisishwan2 <feel000617@gmail.com>
2023-10-03 22:49:24 +0900

    [FEAT] SecurityContextHolder로 부터 회원 조회

---
+++ src/main/java/farmSystem/closeUp/service/UserService.java
@@ -0,0 +1,27 @@
+package farmSystem.closeUp.service;
+
+import farmSystem.closeUp.config.security.SecurityUtils;
+import farmSystem.closeUp.domain.User;
+import farmSystem.closeUp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+
+    private UserRepository userRepository;
+
+
+    public Long getCurrentUserId() {
+        return SecurityUtils.getCurrentUserId();
+    }
+
+    public User getCurrentUser() {
+        return userRepository
+                .findById(SecurityUtils.getCurrentUserId())
+                .orElseThrow(() -> new RuntimeException("사용자를 못찾음"));
+    }
+}
