650ec24ca5dee1ec64d5f4486b29dc0a1bf4903e
thisishwan2 <feel000617@gmail.com>
2023-10-05 22:06:17 +0900

    [FEAT] Redis 클래스 생성

---
+++ src/main/java/farmSystem/closeUp/config/redis/RedisUtils.java
@@ -0,0 +1,28 @@
+package farmSystem.closeUp.config.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+@Component
+public class RedisUtils {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void setData(String key, String value,Long expiredTime){
+        redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
+    }
+
+    public String getData(String key){
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteData(String key){
+        redisTemplate.delete(key);
+    }
+}
