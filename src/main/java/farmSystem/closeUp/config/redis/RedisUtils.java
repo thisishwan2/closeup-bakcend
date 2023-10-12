04dba505f48e2d1cd9216b7b99aacc7e2770ba6d
thisishwan2 <feel000617@gmail.com>
2023-10-12 14:32:21 +0900

    [FEAT] RefreshToken 저장소 Redis 추가

--- src/main/java/farmSystem/closeUp/config/redis/RedisUtils.java
+++ src/main/java/farmSystem/closeUp/config/redis/RedisUtils.java
@@ -1,28 +1,45 @@
 package farmSystem.closeUp.config.redis;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
-@Service
-@RequiredArgsConstructor
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class RedisUtils {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final StringRedisTemplate redisTemplate;
 
-    public void setData(String key, String value,Long expiredTime){
-        redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
+    // getData : key 입력 시, value 리턴 / value 입력 시, key 리턴
+    @Transactional(readOnly = true)
+    public String getData(String key) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        if (values.get(key) == null) {
+            return "false";
+        }
+        return (String) values.get(key);
     }
 
-    public String getData(String key){
-        return (String) redisTemplate.opsForValue().get(key);
+    public void setData(String key, String value) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, value);
     }
 
-    public void deleteData(String key){
+    public void setData(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    // data 삭제
+    public void deleteData(String key) {
         redisTemplate.delete(key);
     }
 }
