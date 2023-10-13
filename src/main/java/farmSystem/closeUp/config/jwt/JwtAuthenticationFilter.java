1eac60c83c328f799275e84322fbfcc3bfa8b5f6
thisishwan2 <feel000617@gmail.com>
2023-10-13 21:25:17 +0900

    [FIX] 토큰 검사 수정

--- src/main/java/farmSystem/closeUp/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/farmSystem/closeUp/config/jwt/JwtAuthenticationFilter.java
@@ -1,7 +1,9 @@
 package farmSystem.closeUp.config.jwt;
 
+import farmSystem.closeUp.config.redis.RedisUtils;
 import farmSystem.closeUp.domain.User;
 import farmSystem.closeUp.repository.UserRepository;
+import farmSystem.closeUp.service.UserService;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -18,6 +20,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.naming.AuthenticationException;
 import java.io.IOException;
 
 @Slf4j
@@ -26,44 +29,33 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserRepository userRepository;
+    private final RedisUtils redisUtils;
+    private final UserService userService;
+
     private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        return request.getRequestURI().contains("/main") || request.getRequestURI().contains("/login-success") || request.getRequestURI().contains("/login");
+        return request.getRequestURI().contains("/main") || request.getRequestURI().contains("/login-success") || request.getRequestURI().contains("/login")
+                || request.getRequestURI().contains("/reissue");
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         log.info("JwtAuthFilter 동작");
-
-        String refreshToken = jwtService.extractRefreshToken(request)
-                .filter(jwtService::validateToken)
-                .orElse(null);
-
-//        // 리프레시 토큰이 요청 헤더에 존재했다면, 사용자가 AccessToken이 만료되어서
-//        // RefreshToken까지 보낸 것이므로 리프레시 토큰이 DB의 리프레시 토큰과 일치하는지 판단 후,
-//        // 일치한다면 AccessToken을 재발급해준다.
-//        if (refreshToken != null) {
-//            checkRefreshTokenAndReIssueAccessToken(response, refreshToken);
-//            return; // RefreshToken을 보낸 경우에는 AccessToken을 재발급 하고 인증 처리는 하지 않게 하기위해 바로 return으로 필터 진행 막기
-//        }
-//
-//        // RefreshToken이 없거나 유효하지 않다면, AccessToken을 검사하고 인증을 처리하는 로직 수행
-//        // AccessToken이 없거나 유효하지 않다면, 인증 객체가 담기지 않은 상태로 다음 필터로 넘어가기 때문에 403 에러 발생
-//        // AccessToken이 유효하다면, 인증 객체가 담긴 상태로 다음 필터로 넘어가기 때문에 인증 성공
-//        if (refreshToken == null) {
-//            checkAccessTokenAndAuthentication(request, response, filterChain);
-//        }
+        String requestURI = request.getRequestURI();
 
         //accessToken 꺼내기
-        String accessToken = jwtService.extractAccessToken(request).get();
-        log.info("accesstoken = "+accessToken);
-        String requestURI = request.getRequestURI();
+//        String accessToken = jwtService.extractAccessToken(request).get();
+        String accessToken = jwtService.extractAccessToken(request)
+                .filter(jwtService::validateToken)
+                .orElse(null);
+        log.info("accesstoken = "+ accessToken);
 
         // 토큰 검사 생략(permitall URL의 경우 토큰 검사 통과)
         if (!StringUtils.hasText(accessToken)) {
+            log.info("토큰 검사 생략");
             doFilter(request, response, filterChain);
             return;
         }
@@ -81,22 +73,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    /**
-     *  [리프레시 토큰으로 유저 정보 찾기 & 액세스 토큰/리프레시 토큰 재발급 메소드]
-     *  파라미터로 들어온 헤더에서 추출한 리프레시 토큰으로 DB에서 유저를 찾고, 해당 유저가 있다면
-     *  JwtService.createAccessToken()으로 AccessToken 생성,
-     *  reIssueRefreshToken()로 리프레시 토큰 재발급 & DB에 리프레시 토큰 업데이트 메소드 호출
-     *  그 후 JwtService.sendAccessTokenAndRefreshToken()으로 응답 헤더에 보내기
-     */
-//    public void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken) {
-//        userRepository.findByRefreshToken(refreshToken)
-//                .ifPresent(user -> {
-//                    String reIssuedRefreshToken = jwtService.createRefreshToken();
-//                    jwtService.sendAccessAndRefreshToken(response, jwtService.createAccessToken(user.),
-//                            reIssuedRefreshToken);
-//                });
-//    }
-
 
     /**
      * [액세스 토큰 체크 & 인증 처리 메소드]
@@ -111,8 +87,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         log.info("checkAccessTokenAndAuthentication() 호출");
         jwtService.extractAccessToken(request)
                 .filter(jwtService::validateToken)
-                .ifPresent(accessToken -> jwtService.getUserEmail(accessToken)
-                        .ifPresent(email -> userRepository.findByEmail(email)
+                .ifPresent(accessToken -> jwtService.getUserId(accessToken)
+                        .ifPresent(userId -> userRepository.findById(userId)
                                 .ifPresent(this::saveAuthentication)));
 
         filterChain.doFilter(request, response);
@@ -134,13 +110,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
      * setAuthentication()을 이용하여 위에서 만든 Authentication 객체에 대한 인증 허가 처리
      */
     public void saveAuthentication(User myUser) {
+        log.info("인증객체 저장 시작");
         String password = myUser.getPassword();
         if (password == null) { // 소셜 로그인 유저의 비밀번호 임의로 설정 하여 소셜 로그인 유저도 인증 되도록 설정
             password = PasswordUtil.generateRandomPassword();
         }
 
         UserDetails userDetailsUser = org.springframework.security.core.userdetails.User.builder()
-                .username(myUser.getEmail())
+                .username(String.valueOf(myUser.getUserId()))
                 .password(password)
                 .roles(String.valueOf(myUser.getUserRole().GUEST))
                 .build();
