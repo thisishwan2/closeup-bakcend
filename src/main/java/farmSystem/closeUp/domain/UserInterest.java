131659ae31ce259c49e1aed4b72ecacdab93732e
thisishwan2 <feel000617@gmail.com>
2023-10-16 11:49:33 +0900

    [FEAT] 회원 관심사 엔티티 추가

---
+++ src/main/java/farmSystem/closeUp/domain/UserInterest.java
@@ -0,0 +1,25 @@
+package farmSystem.closeUp.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserInterest extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_interest_id")
+    private Long userInterestId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user; //일반유저, 크리에이터
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interest_id")
+    private Interest interest;
+}
