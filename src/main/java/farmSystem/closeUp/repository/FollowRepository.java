58e6fe62564b3dc33150872a809c9bdd26b206ee
thisishwan2 <feel000617@gmail.com>
2023-11-03 21:28:33 +0900

    [FEAT] userId로 Follow 리스트 조회메서드 추가

---
+++ src/main/java/farmSystem/closeUp/repository/FollowRepository.java
@@ -0,0 +1,10 @@
+package farmSystem.closeUp.repository;
+
+import farmSystem.closeUp.domain.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    List<Follow> findByUserUserId(Long userId);
+}
