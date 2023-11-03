b31f0ac01967ff1dbe23b6b99f9a364f767d7982
thisishwan2 <feel000617@gmail.com>
2023-11-03 21:29:24 +0900

    [FEAT] 키워드 검색 조회 메서드 및 플랫폼으로 조회 메서드 추가

--- src/main/java/farmSystem/closeUp/repository/UserRepository.java
+++ src/main/java/farmSystem/closeUp/repository/UserRepository.java
@@ -2,8 +2,10 @@ package farmSystem.closeUp.repository;
 
 import farmSystem.closeUp.domain.SocialType;
 import farmSystem.closeUp.domain.User;
+import farmSystem.closeUp.domain.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -16,4 +18,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
      추가 정보를 입력받아 회원 가입을 진행할 때 소셜 타입, 식별자로 해당 회원을 찾기 위한 메소드
      ***/
     Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+
+    List<User> findByNickNameContainingAndUserRole(String searchName, UserRole userRole);
+
+    List<User> findByPlatform(Long platformId);
+
 }
