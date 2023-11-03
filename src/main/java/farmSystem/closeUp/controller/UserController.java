5463ef7359e98a480994b1131d51dfd6cf9d8af1
thisishwan2 <feel000617@gmail.com>
2023-11-03 21:15:03 +0900

    [FEAT] 크리에이터 검색 및 플랫폼 별 크리에이터 조회 컨트롤러 추가

--- src/main/java/farmSystem/closeUp/controller/UserController.java
+++ src/main/java/farmSystem/closeUp/controller/UserController.java
@@ -3,13 +3,14 @@ package farmSystem.closeUp.controller;
 
 import farmSystem.closeUp.common.CommonResponse;
 import farmSystem.closeUp.dto.response.TokenResponse;
+import farmSystem.closeUp.dto.user.response.GetSearchCreatorResponse;
 import farmSystem.closeUp.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -23,5 +24,17 @@ public class UserController {
     public CommonResponse<TokenResponse> reIssueToken(@RequestHeader("Authorization-refresh") @Valid final String refreshToken) {
         TokenResponse tokenResponse = userService.reIssueToken(refreshToken);
         return CommonResponse.success(tokenResponse);
+    }
+
+    // 크리에이터 이름 키워드 검색
+    @GetMapping("/user/search")
+    public CommonResponse<List<GetSearchCreatorResponse>> searchCreatorByKeyword(@RequestParam("keyword") String keyword) {
+        return CommonResponse.success(userService.searchCreatorByKeyword(keyword));
+    }
+
+    // 특정 플랫폼에 포함된 크리에이터 조회
+    @GetMapping("/user/category/{platformId}")
+    public CommonResponse<List<GetSearchCreatorResponse>> searchCreatorByPlatform(@PathVariable("platformId") Long platformId) {
+        return CommonResponse.success(userService.searchCreatorByPlatform(platformId));
     }
 }
