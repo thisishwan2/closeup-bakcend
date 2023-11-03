73dd3a8ada4c26206262d19b2dc954978c8a6916
thisishwan2 <feel000617@gmail.com>
2023-11-03 21:28:46 +0900

    [FEAT] PlatformRepository 추가

---
+++ src/main/java/farmSystem/closeUp/repository/PlatformRepository.java
@@ -0,0 +1,8 @@
+package farmSystem.closeUp.repository;
+
+import farmSystem.closeUp.domain.Platform;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlatformRepository extends JpaRepository<Platform, Long> {
+
+}
