f569eea58f2b92f4bce9515302f641ffd11e33ba
thisishwan2 <feel000617@gmail.com>
2023-11-03 21:17:27 +0900

    [FIX] 시간 필드 LocalDateTime으로 변경 및 카테고리 연관관계 수정

--- src/main/java/farmSystem/closeUp/domain/RaffleProduct.java
+++ src/main/java/farmSystem/closeUp/domain/RaffleProduct.java
@@ -5,7 +5,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,20 +24,30 @@ public class RaffleProduct extends BaseEntity{
     private Long raffleProductId;
 
     private String title;
-    private String startDate;
-    private String endDate;
+
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+    private LocalDateTime startDate;
+
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+    private LocalDateTime endDate;
+
     private String content;
     private Long winnerCount;
     private Long rafflePrice;
     private String address;
-    private String winningDate;
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+    private LocalDateTime winningDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_id")
     private User creator; //크리에이터
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="category_id")
+    private Category category;
+
     @Builder
-    public RaffleProduct(Long raffleProductId, String title, String startDate, String endDate, String content, Long winnerCount, Long rafflePrice, String address, String winningDate) {
+    public RaffleProduct(Long raffleProductId, String title, LocalDateTime startDate, LocalDateTime endDate, String content, Long winnerCount, Long rafflePrice, String address, LocalDateTime winningDate) {
         this.raffleProductId = raffleProductId;
         this.title = title;
         this.startDate = startDate;
