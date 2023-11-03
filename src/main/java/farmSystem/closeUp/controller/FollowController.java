92aadc4c477a4babf4a7ce45a3480c8059b32742
thisishwan2 <feel000617@gmail.com>
2023-11-03 21:13:21 +0900

    [FEAT] 회원이 팔로우 한 크리에이터 조회 컨트롤러 추가

---
+++ src/main/java/farmSystem/closeUp/controller/FollowController.java
@@ -0,0 +1,27 @@
+package farmSystem.closeUp.controller;
+
+import farmSystem.closeUp.common.CommonResponse;
+import farmSystem.closeUp.dto.follow.response.GetFollowingResponse;
+import farmSystem.closeUp.service.FollowService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class FollowController {
+
+    private final FollowService followService;
+
+    // 회원이 팔로우한 크리에이터 조회
+    @GetMapping("/user/{userId}/following")
+    public CommonResponse<List<GetFollowingResponse>> getFollowingList(@PathVariable("userId") Long userId) {
+        return CommonResponse.success(followService.getFollowingList(userId));
+    }
+
+}
