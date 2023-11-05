fa7d04399438c954e7f558b990c25d7f4b66352b
thisishwan2 <feel000617@gmail.com>
2023-11-05 19:03:21 +0900

    [RENAME) 위치 변경

--- src/main/java/farmSystem/closeUp/repository/PlatformRepository.java
+++
@@ -1,8 +0,0 @@
-package farmSystem.closeUp.repository;
-
-import farmSystem.closeUp.domain.Platform;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlatformRepository extends JpaRepository<Platform, Long> {
-
-}
