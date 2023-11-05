a43d8a70d9cb6592f3e19463066dfd58dd629ca4
thisishwan2 <feel000617@gmail.com>
2023-11-05 19:05:51 +0900

    [RENAME] 이동

---
+++ src/main/java/farmSystem/closeUp/dto/user/response/PostTokenReissueResponse.java
@@ -0,0 +1,17 @@
+package farmSystem.closeUp.dto.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PostTokenReissueResponse {
+
+    private final String accessToken;
+    private final String refreshToken;
+
+    public static PostTokenReissueResponse of(String accessToken, String refreshToken){
+        return PostTokenReissueResponse.builder().accessToken(accessToken).refreshToken(refreshToken).build();
+    }
+}
