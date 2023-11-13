70908cb2259ad1450e7beec5505fbfcf45c1bf4e
thisishwan2 <feel000617@gmail.com>
2023-11-13 18:28:30 +0900

    [FEAT] 기본 생성자 추가

--- src/main/java/farmSystem/closeUp/dto/request/UserInfoRequest.java
+++ src/main/java/farmSystem/closeUp/dto/request/UserInfoRequest.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserInfoRequest {
     @NotBlank(message = "닉네임 입력은 필수입니다.")
