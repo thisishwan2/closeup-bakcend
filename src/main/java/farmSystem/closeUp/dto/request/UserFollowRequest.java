9264e206a8b2d61759c45c54a9660c82235a0cad
thisishwan2 <feel000617@gmail.com>
2023-11-13 18:42:01 +0900

    [DELETE] 테스트 코드 삭제

--- src/main/java/farmSystem/closeUp/dto/request/UserFollowRequest.java
+++
@@ -1,8 +0,0 @@
-package farmSystem.closeUp.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class UserFollowRequest {
-    Long[] creatorId;
-}
--- src/main/java/farmSystem/closeUp/dto/request/UserInterestRequest.java
+++
@@ -1,8 +0,0 @@
-package farmSystem.closeUp.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class UserInterestRequest {
-    Long[] genreId;
-}
