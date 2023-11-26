92319ecbd85c4a497310fd67a18e0f5b1bb39fc4
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-26 21:51:05 +0900

    [FEAT] 방명록 작성 컨트롤러 추가

--- src/main/java/farmSystem/closeUp/controller/GuestBookController.java
+++ src/main/java/farmSystem/closeUp/controller/GuestBookController.java
@@ -1,15 +1,15 @@
 package farmSystem.closeUp.controller;
 
 import farmSystem.closeUp.common.CommonResponse;
+import farmSystem.closeUp.dto.guestbook.request.PostGuestBooksRequest;
 import farmSystem.closeUp.dto.guestbook.response.GetGuestBooksResponse;
+import farmSystem.closeUp.dto.guestbook.response.PostGuestBooksResponse;
 import farmSystem.closeUp.service.GuestBookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -24,4 +24,11 @@ public class GuestBookController {
         return CommonResponse.success(guestBookService.getGuestBooks(creatorId, pageable));
     }
 
+    // 크리에이터 방명록 작성
+    @PostMapping("/user/{creatorId}/guestbooks")
+    public CommonResponse<PostGuestBooksResponse> postGuestbook (
+            @PathVariable("creatorId") Long creatorId,
+            @RequestBody PostGuestBooksRequest request) {
+        return CommonResponse.success(guestBookService.postGuestBook(creatorId, request));
+    }
 }
