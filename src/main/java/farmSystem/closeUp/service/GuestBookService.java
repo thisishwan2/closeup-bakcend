12624de608bc39cfc30886653c851f5479de1761
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-26 21:51:38 +0900

    [FEAT] 방명록 작성 서비스 로직 추가

--- src/main/java/farmSystem/closeUp/service/GuestBookService.java
+++ src/main/java/farmSystem/closeUp/service/GuestBookService.java
@@ -1,8 +1,15 @@
 package farmSystem.closeUp.service;
 
+import farmSystem.closeUp.common.CustomException;
+import farmSystem.closeUp.common.Result;
+import farmSystem.closeUp.domain.GuestBook;
+import farmSystem.closeUp.domain.User;
+import farmSystem.closeUp.dto.guestbook.request.PostGuestBooksRequest;
 import farmSystem.closeUp.dto.guestbook.response.GetGuestBooksResponse;
+import farmSystem.closeUp.dto.guestbook.response.PostGuestBooksResponse;
 import farmSystem.closeUp.repository.guestbook.GuestBookRepository;
 import farmSystem.closeUp.repository.guestbook.GuestBookRepositoryImpl;
+import farmSystem.closeUp.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -19,6 +26,8 @@ public class GuestBookService {
 
     private final GuestBookRepository guestBookRepository;
     private final GuestBookRepositoryImpl guestBookRepositoryImpl;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
     @Transactional
     public Slice<GetGuestBooksResponse> getGuestBooks(Long creatorId, Pageable pageable) {
@@ -28,4 +37,25 @@ public class GuestBookService {
 
         return findGuestBooks;
     }
+
+    @Transactional
+    public PostGuestBooksResponse postGuestBook(Long creatorId, PostGuestBooksRequest request) {
+        User user = userService.getCurrentUser();
+
+        GuestBook guestBook = GuestBook.builder()
+                .content(request.getContent())
+                .build();
+
+        User creator = userRepository.findById(creatorId).orElseThrow(() -> new CustomException(Result.NOTFOUND_USER));
+        guestBook.setUser(user);
+        guestBook.setCreator(creator);
+
+        guestBookRepository.save(guestBook);
+
+        return PostGuestBooksResponse.builder()
+                .guestBookId(guestBook.getGuestBookId())
+                .content(guestBook.getContent())
+                .build();
+    }
+
 }
