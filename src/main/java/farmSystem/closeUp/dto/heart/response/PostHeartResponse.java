8e8c0b92a1db2191d38b4798499f8c095d4d6796
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-27 17:58:34 +0900

    [FEAT] 크리에이터 - 방명록 좋아요(반응) Response 생성

---
+++ src/main/java/farmSystem/closeUp/dto/heart/response/PostHeartResponse.java
@@ -0,0 +1,12 @@
+package farmSystem.closeUp.dto.heart.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PostHeartResponse {
+
+    private Long heartId;
+
+}
