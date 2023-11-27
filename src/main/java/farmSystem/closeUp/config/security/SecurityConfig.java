9ae9bee0ee347f89e6ea1cca911a4a54d7d1c03b
thisishwan2 <feel000617@gmail.com>
2023-11-27 20:24:36 +0900

    [FEAT] 헬스체킹 추가

--- src/main/java/farmSystem/closeUp/config/security/SecurityConfig.java
+++ src/main/java/farmSystem/closeUp/config/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                 // 특정 URL에 대한 권한 설정
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/login-success-test/**").permitAll()
-                        .requestMatchers("/token/reissue", "/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
+                        .requestMatchers("/health","/token/reissue", "/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
                         .requestMatchers("/user/sign-up/**").hasAnyRole(String.valueOf(UserRole.GUEST), String.valueOf(UserRole.SIGNUP_USER), String.valueOf(UserRole.FOLLOWED_USER), String.valueOf(UserRole.INTERESTED_USER))
                         .requestMatchers("/user/**").hasRole(String.valueOf(UserRole.USER))
                         .requestMatchers("/creator/**").hasRole("CREATOR")
