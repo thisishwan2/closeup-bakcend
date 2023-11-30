00d8e21cfd48f7d7ede134116af236672cff647a
thisishwan2 <feel000617@gmail.com>
2023-11-30 21:29:45 +0900

    [FEAT] 회원가입 request dto 추가

---
+++ src/main/java/farmSystem/closeUp/dto/user/request/PostCreatorInfoRequest.java
@@ -0,0 +1,21 @@
+package farmSystem.closeUp.dto.user.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class PostCreatorInfoRequest {
+    @NotBlank(message = "닉네임 입력은 필수입니다.")
+    private String nickname;
+    @NotBlank(message = "주소 입력은 필수입니다.")
+    private String address;
+    @NotBlank(message = "휴대전화번호 입력은 필수입니다.")
+    private String phoneNumber;
+
+    @NotBlank(message = "성별 입력은 필수입니다.")
+    private String gender;
+    @NotBlank(message = "생일 입력은 필수입니다.")
+    private String birthday;
+    @NotBlank(message = "한줄소개는 필수입니다.")
+    private String profileComment;
+}
