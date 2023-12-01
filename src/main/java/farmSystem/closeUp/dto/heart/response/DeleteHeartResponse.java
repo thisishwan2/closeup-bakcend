eee60142241acea82e3978453c660e7d3d9be812
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-12-01 23:29:12 +0900

    [FEAT] 크리에이터 - 방명록 좋아요 취소 Response 추가

---
+++ src/main/java/farmSystem/closeUp/dto/heart/response/DeleteHeartResponse.java
@@ -0,0 +1,10 @@
+package farmSystem.closeUp.dto.heart.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class DeleteHeartResponse {
+    private Long heartId;
+}
