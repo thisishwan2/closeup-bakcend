4c9acace49803c40228d122faa3c4618e7e76cd3
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-12-02 00:48:55 +0900

    [FEAT] 크리에이터 - 방명록 조회 컨트롤러 추가

--- src/main/java/farmSystem/closeUp/controller/GuestBookController.java
+++ src/main/java/farmSystem/closeUp/controller/GuestBookController.java
@@ -21,13 +21,13 @@ public class GuestBookController {
 
     private final GuestBookService guestBookService;
 
-    // 크리에이터 방명록 조회(무한 스크롤)
+    // 크리에이터 방명록 조회(무한 스크롤) - 유저
     @GetMapping("/user/{creatorId}/guestbooks")
     public CommonResponse<Slice<GetGuestBooksResponse>> getGuestBooks(@PathVariable("creatorId") Long creatorId, Pageable pageable) {
         return CommonResponse.success(guestBookService.getGuestBooks(creatorId, pageable));
     }
 
-    // 크리에이터 방명록 작성
+    // 크리에이터 방명록 작성 - 유저
     @PostMapping("/user/{creatorId}/guestbooks")
     public CommonResponse<PostGuestBooksResponse> postGuestbook (
             @PathVariable("creatorId") Long creatorId,
@@ -35,7 +35,7 @@ public class GuestBookController {
         return CommonResponse.success(guestBookService.postGuestBook(creatorId, request));
     }
 
-    // 크리에이터 방명록 수정
+    // 크리에이터 방명록 수정 - 유저
     @PatchMapping("/user/guestbooks/{guestbookId}")
     public CommonResponse<PatchGuestBooksResponse> patchGuestBook (
             @PathVariable("guestbookId") Long guestbookId,
@@ -49,6 +49,14 @@ public class GuestBookController {
             @PathVariable("guestbookId") Long guestbookId) {
         return CommonResponse.success(guestBookService.deleteGuestBook(guestbookId));
     }
+
+
+    // 크리에이터 방명록 조회(무한 스크롤) - 크리에이터
+    @GetMapping("/creator/{creatorId}/guestbooks")
+    public CommonResponse<Slice<GetGuestBooksResponse>> getGuestBooksCreator(@PathVariable("creatorId") Long creatorId, Pageable pageable) {
+        return CommonResponse.success(guestBookService.getGuestBooksCreator(creatorId, pageable));
+    }
+
 
     // 크리에이터 방명록 삭제 - 크리에이터
     @DeleteMapping("/creator/guestbooks/{guestbookId}")
