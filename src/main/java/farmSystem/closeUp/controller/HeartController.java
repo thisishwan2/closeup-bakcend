c2f2d40e838cd3273af905ac708fcc5d89da937f
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-12-01 23:29:00 +0900

    [FEAT] 크리에이터 - 방명록 좋아요 취소 컨트롤러 추가

--- src/main/java/farmSystem/closeUp/controller/HeartController.java
+++ src/main/java/farmSystem/closeUp/controller/HeartController.java
@@ -1,10 +1,12 @@
 package farmSystem.closeUp.controller;
 
 import farmSystem.closeUp.common.CommonResponse;
+import farmSystem.closeUp.dto.heart.response.DeleteHeartResponse;
 import farmSystem.closeUp.dto.heart.response.PostHeartResponse;
 import farmSystem.closeUp.service.HeartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,4 +22,11 @@ public class HeartController {
     public CommonResponse<PostHeartResponse> postHeart(@PathVariable("guestbookId") Long guestbookId) {
         return CommonResponse.success(heartService.postHeart(guestbookId));
     }
+
+    // 크리에이터 - 방명록 반응 삭제
+    @DeleteMapping("/creator/guestbooks/heart/{heartId}")
+    public CommonResponse<DeleteHeartResponse> deleteHeart(@PathVariable("heartId") Long heartId) {
+        return CommonResponse.success(heartService.deleteHeart(heartId));
+    }
+
 }
