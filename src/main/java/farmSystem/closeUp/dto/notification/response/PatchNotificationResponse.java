9b5e4afbf3391d8e543b07a15f8518836f1dad83
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-12-31 22:07:35 +0900

    [FEAT] 크리에이터 - 공지사항 수정 response 추가

---
+++ src/main/java/farmSystem/closeUp/dto/notification/response/PatchNotificationResponse.java
@@ -0,0 +1,20 @@
+package farmSystem.closeUp.dto.notification.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PatchNotificationResponse {
+    private Long notificationId;
+    private String title;
+    private String content;
+
+    public static PostNotificationResponse of(Long notificationId, String title, String content) {
+        return PostNotificationResponse.builder()
+                .notificationId(notificationId)
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
