360aaa2610587851ac96d718213c308d97eb73df
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-12-31 21:43:11 +0900

    [FEAT] 크리에이터 - 공지사항 조회 컨트롤러 추가

--- src/main/java/farmSystem/closeUp/controller/NotificationController.java
+++ src/main/java/farmSystem/closeUp/controller/NotificationController.java
@@ -18,9 +18,15 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
-    // 크리에이터 공지사항 조회(무한 스크롤)
+    // 크리에이터 공지사항 조회(무한 스크롤) - 유저
     @GetMapping("/user/{creatorId}/notifications")
     public CommonResponse<Slice<GetNotificationsResponse>> getNotifications(@PathVariable("creatorId") Long creatorId, Pageable pageable) {
         return CommonResponse.success(notificationService.getNotifications(creatorId, pageable));
+    }
+
+    // 크리에이터 공지사항 조회(무한 스크롤) - 크리에이터
+    @GetMapping("/creator/{creatorId}/notifications")
+    public CommonResponse<Slice<GetNotificationsResponse>> getNotificationsCreator(@PathVariable("creatorId") Long creatorId, Pageable pageable) {
+        return CommonResponse.success(notificationService.getNotificationsCreator(creatorId, pageable));
     }
 }
