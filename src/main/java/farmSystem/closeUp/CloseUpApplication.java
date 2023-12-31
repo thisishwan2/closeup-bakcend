2575204cb3c7a2e418bb9ead531068bec14bfede
thisishwan2 <feel000617@gmail.com>
2023-12-31 17:52:19 +0900

    feat: 스케줄링 사용

--- src/main/java/farmSystem/closeUp/CloseUpApplication.java
+++ src/main/java/farmSystem/closeUp/CloseUpApplication.java
@@ -2,8 +2,10 @@ package farmSystem.closeUp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CloseUpApplication {
 
 	public static void main(String[] args) {
