74725a4979c8178d87a5510ed44eabe00bd35455
thisishwan2 <feel000617@gmail.com>
2024-01-08 21:34:04 +0900

    remove: 주석 제거

--- src/main/java/farmSystem/closeUp/config/CorsConfig.java
+++ src/main/java/farmSystem/closeUp/config/CorsConfig.java
@@ -17,8 +17,6 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("*");
-        //config.addAllowedOrigin("http://localhost:3000");
-        //config.addAllowedOrigin("http://localhost:80")
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
