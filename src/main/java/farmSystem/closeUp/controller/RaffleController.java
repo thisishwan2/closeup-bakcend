b13adab7d8c41cece592565b8f92a07d9626a9cc
thisishwan2 <feel000617@gmail.com>
2024-01-08 23:29:01 +0900

    feat: 주석 추가

--- src/main/java/farmSystem/closeUp/controller/RaffleController.java
+++ src/main/java/farmSystem/closeUp/controller/RaffleController.java
@@ -22,16 +22,19 @@ public class RaffleController {
 
     private final RaffleService raffleService;
 
+    // 전체 응모 래플 목록 조회
     @GetMapping("/user/raffles")
     public CommonResponse<List<GetRafflesUserResponse>> getRafflesAll() {
         return CommonResponse.success(raffleService.getRafflesUserAll());
     }
 
+    // 당첨된 래플 목록 조회
     @GetMapping("/user/raffles/winning")
     public CommonResponse<List<GetRafflesUserResponse>> getRafflesWinning() {
         return CommonResponse.success(raffleService.getRafflesUserWinning());
     }
 
+    // 래플 응모 내역 상세 조회
     @GetMapping("/user/raffles/{raffleId}")
     public CommonResponse<GetUserRaffleDetailResponse> getRaffleDetail(@PathVariable("raffleId") Long raffleId) {
         return CommonResponse.success(raffleService.getRaffleDetail(raffleId));
