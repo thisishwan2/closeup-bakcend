f12ef2d9a59c9ba20063bb3ca99214b8fdead430
thisishwan2 <feel000617@gmail.com>
2024-01-08 22:46:53 +0900

    feat: repository 추가

---
+++ src/main/java/farmSystem/closeUp/repository/winningProduct/WinningProductRepository.java
@@ -0,0 +1,7 @@
+package farmSystem.closeUp.repository.winningProduct;
+
+import farmSystem.closeUp.domain.WinningProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WinningProductRepository extends JpaRepository<WinningProduct, Long> {
+}
