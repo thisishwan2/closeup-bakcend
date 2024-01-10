78ca695e921fc6b2c679dfc74a48d49124829510
thisishwan2 <feel000617@gmail.com>
2024-01-10 18:19:07 +0900

    fix: 생성자 어노테이션 추가

--- src/main/java/farmSystem/closeUp/dto/raffleProduct/request/PostCreateRaffleProductRequest.java
+++ src/main/java/farmSystem/closeUp/dto/raffleProduct/request/PostCreateRaffleProductRequest.java
@@ -1,13 +1,17 @@
 package farmSystem.closeUp.dto.raffleProduct.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostCreateRaffleProductRequest {
     private String title;
     private LocalDate startDate;
