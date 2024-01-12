ae9f37265be45c855d6e6b90d1f622819eee8f80
thisishwan2 <feel000617@gmail.com>
2024-01-12 18:52:28 +0900

    fix: jpa 메서드 수정

--- src/main/java/farmSystem/closeUp/repository/raffleProduct/RaffleProductRepository.java
+++ src/main/java/farmSystem/closeUp/repository/raffleProduct/RaffleProductRepository.java
@@ -2,13 +2,18 @@ package farmSystem.closeUp.repository.raffleProduct;
 
 import farmSystem.closeUp.domain.RaffleProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface RaffleProductRepository extends JpaRepository<RaffleProduct, Long>, RaffleProductRepositoryCustom {
 
-    List<RaffleProduct> findByEndDate(LocalDate endDate);
+    List<RaffleProduct> findByWinningDate(LocalDateTime winningDate);
 
-    List<RaffleProduct> findByWinningDate(LocalDate winningDate);
+//    List<RaffleProduct> findByWinningDate(LocalDate winningDate);
+
+//    @Query("SELECT r FROM RaffleProduct r WHERE FUNCTION('to_char', r.winningDate, 'YYYY-MM-DD') = :winningDate")
+//    List<RaffleProduct> findByWinningDate(LocalDate winningDate);
 }
