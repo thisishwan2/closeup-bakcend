7dda911fe5b3ac0116435c65cc3254fb499c160b
thisishwan2 <feel000617@gmail.com>
2024-01-19 23:32:50 +0900

    fix: 크리에이터도 래플에 접근 가능하도록 수정

--- src/main/java/farmSystem/closeUp/config/security/SecurityConfig.java
+++ src/main/java/farmSystem/closeUp/config/security/SecurityConfig.java
@@ -73,6 +73,7 @@ public class SecurityConfig {
                         .requestMatchers("/health","/token/reissue", "/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
                         .requestMatchers("/user/sign-up/**").hasAnyRole(String.valueOf(UserRole.GUEST), String.valueOf(UserRole.SIGNUP_USER), String.valueOf(UserRole.FOLLOWED_USER), String.valueOf(UserRole.INTERESTED_USER))
                         .requestMatchers("/user/**").hasRole(String.valueOf(UserRole.USER))
+                        .requestMatchers("/user/raffle-products", "/user/raffle-products/{raffleProductId}").hasAnyRole(String.valueOf(UserRole.USER), String.valueOf(UserRole.CREATOR))
                         .requestMatchers("/creator/sign-up/**").hasAnyRole(String.valueOf(UserRole.GUEST), String.valueOf(UserRole.SIGNUP_CREATOR))
                         .requestMatchers("/creator/**").hasRole(String.valueOf(UserRole.CREATOR))
                     .anyRequest().authenticated()
